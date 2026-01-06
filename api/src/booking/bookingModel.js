41752274b577d06d65753912c5df28f3777f9e98
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-01-06 13:35:39 +0100

    Completo el Schema de reservas en la base de datos

--- api/src/booking/bookingModel.js
+++ api/src/booking/bookingModel.js
@@ -1,13 +1,48 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const schema = new Schema({
-    room: ObjectId,
-    client: ObjectId,
-    checkInDate: Date,
-    checkOutDate: Date,
-    cancelDate: Date,
-    price: Int32
+const bookingDatabaseSchema = new Schema({
+    room: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    client: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    checkInDate: {
+        type: Date,
+        required: true
+    },
+    checkOutDate: {
+        type: Date,
+        required: true
+    },
+    payDate: {
+        type: Date,
+        default: Date.now()
+    },
+    totalPrice: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    pricePerNight: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    offer: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Finalizada", "Cancelada", "Abierta"],
+        default: "Abierta"
+    },
+    guests: {
+        type: Schema.Types.Number,
+        required: true
+    }
 });
 
-export default mongoose.model('booking', schema)
+export const bookingDatabaseModel = mongoose.model('booking', bookingDatabaseSchema)
