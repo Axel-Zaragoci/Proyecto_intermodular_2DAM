d1d150cf917602ef24041d71f44069347d7ad345
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-01-09 18:43:03 +0100

    Creación función del controlador para obtener todas las reservas

--- api/src/booking/bookingController.js
+++ api/src/booking/bookingController.js
@@ -23,3 +23,23 @@ async function getOneBookingById(req, res) {
         return res.status(500).json({ error: 'Error del servidor' });
     }
 }
+
+/**
+ * @type import("express").RequestHandler
+ * 
+ * @response {200} - Devuelve todas las reservas
+ * @response {404} - No se encontraron reservas
+ * @response {500} - Error del servidor 
+ */
+async function getBookings(req, res) {
+    try {
+        const bookings = await bookingDatabaseModel.find();
+        if (!bookings) return res.status(404).json({ error: 'No se encontraron reservas' });
+        
+        return res.status(200).json(bookings);
+    }
+    catch (error) {
+        console.error('Error al obtener la reserva:', error);
+        return res.status(500).json({ error: 'Error del servidor' });
+    }
+}
