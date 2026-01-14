78a525dcb655723847124ac88f488cc4b70b17e9
IanRM-class <fp.ian.rodriguezmarhuenda@gmail.com>
2026-01-14 17:53:17 +0100

    Creación de modelo de usuarios

---
+++ api/src/users/usersModel.js
@@ -0,0 +1,71 @@
+import { Schema, model } from 'mongoose';
+
+/**
+ * @typedef userSchema
+ * 
+ * @property {import('mongoose').Types.ObjectId} _id - Identificador único creado por MongoDB
+ * @property {String} firstName - Nombre propio del usuario
+ * @property {String} secondName - Apellido/s del usuario
+ * @property {String} password - Contraseña del usuario
+ * @property {String} dni - Documento de identidad del usuario
+ * @property {Date} bornDate - Fecha de nacimiento del usuario
+ * @property {String} cityName - Ciudad de vivienda fiscal
+ * @property {"Hombre" | "Mujer"} gender - Genero del usuario
+ * @property {String} imageRoute - Ruta de la imagen del usuario
+ * @property {"Admin" | "Trabajador" | "Usuario"} rol - Rol del usuario
+ * @property {Boolean} vipStatus - Estado membresia VIP del usuario, "true" cuenta con membresia acctiva, "false" no cuenta con membresia
+ * 
+ * @description Documento correspondiente a los datos en MongoDB sobre el usuario
+*/
+const userDatabaseSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    secondName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: true
+    },
+    dni: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    bornDate: {
+        type: Date,
+        required: true
+    },
+    cityName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    gender: {
+        type: String,
+        enum: ["Hombre", "Mujer"],
+        required: true
+    },
+    imageRoute: {
+        type: String,
+        required: false
+    },
+    rol: {
+        type: String,
+        enum: ["Admin", "Trabajador", "Usuario"],
+        default: "Usuario"
+    },
+    vipStatus: {
+        type: Boolean,
+        default: false
+    }
+});
+
+export const userDatabaseModel = model('user', userDatabaseSchema)
