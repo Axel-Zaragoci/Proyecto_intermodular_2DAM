d7ed196bd41558e054b9c01c51a6ba102c479242
IanRM-class <fp.ian.rodriguezmarhuenda@email.com>
2026-01-15 01:19:33 +0100

    Añadidas validaciones de Usuario y renombre de variables

--- api/src/users/usersModel.js
+++ api/src/users/usersModel.js
@@ -5,10 +5,10 @@ import { Schema, model } from 'mongoose';
  * 
  * @property {import('mongoose').Types.ObjectId} _id - Identificador único creado por MongoDB
  * @property {string} firstName - Nombre propio del usuario
- * @property {string} secondName - Apellido/s del usuario
+ * @property {string} lastName - Apellido/s del usuario
  * @property {string} password - Contraseña del usuario
  * @property {string} dni - Documento de identidad del usuario
- * @property {Date} bornDate - Fecha de nacimiento del usuario
+ * @property {Date} birthDate - Fecha de nacimiento del usuario
  * @property {string} cityName - Ciudad de vivienda fiscal
  * @property {"Hombre" | "Mujer"} gender - Genero del usuario
  * @property {string} imageRoute - Ruta de la imagen del usuario
@@ -23,7 +23,7 @@ const userDatabaseSchema = new Schema({
         required: true,
         trim: true
     },
-    secondName: {
+    lastName: {
         type: String,
         required: true,
         trim: true
@@ -39,7 +39,7 @@ const userDatabaseSchema = new Schema({
         unique: true,
         trim: true
     },
-    bornDate: {
+    birthDate: {
         type: Date,
         required: true
     },
@@ -71,12 +71,12 @@ const userDatabaseSchema = new Schema({
 export const userDatabaseModel = model('user', userDatabaseSchema)
 
 class UserEntryData {
-    constructor(firstName, secondName, password, dni, bornDate, cityName, gender, imageRoute, rol, vipStatus) {
+    constructor(firstName, lastName, password, dni, birthDate, cityName, gender, imageRoute, rol, vipStatus) {
         this.firstName = firstName
-        this.secondName = secondName
+        this.lastName = lastName
         this.password = password
         this.dni = dni
-        this.bornDate = bornDate
+        this.birthDate = birthDate
         this.cityName = cityName
         this.gender = gender
         this.imageRoute = imageRoute
@@ -87,22 +87,29 @@ class UserEntryData {
     }
 
     validate() {
-        //Validar que esten el nombre/apellido
-        //Validar contraseña
-        //Validar DNI
-        //Validar fecha nacimiento
-        //Validar genero
+        if (!this.firstName || !this.lastName) throw new Error("Nombre y Apellido no pueden estar vacíos.");
+        if(/\d/.test(this.firstName) || /\d/.test(this.lastName)) throw new Error("Nombre y Apellido no pueden contener números.");
+        if(this.password.length < 8) throw new Error("La contraseña tiene que contener al menos de 8 caracteres.");
+        if(this.birthDate.getTime() < Date.now() - 504921600000) throw new Error("Tienes que ser mayor de 16 años.")
+        if(!["Hombre", "Mujer"].includes(this.gender)) throw new Error("Seleccione un Genero");
+        if(!/^\d{8}[a-zA-Z]$/.test(this.dni)) throw new Error("DNI Incorrecto.");
+        
+        let letterNum = parseInt(this.dni.slice(0,8));
+        const letterDni = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+        if(this.dni[8] !== letterDni[letterNum%23]) throw new Error("DNI Incorrecto");
+        
         this.ready = true
     }
 
+
     toDocument() {
-        if(!this.ready) throw new Error("Completa la creacción de usuario correctamente.")
+        if(!this.ready) throw new Error("Completa la creación de usuario correctamente.")
         return new userDatabaseModel({
             firstName: this.firstName,
-            secondName: this.secondName,
+            lastName: this.lastName,
             password: this.password,
             dni: this.dni,
-            bornDate: this.bornDate,
+            birthDate: this.birthDate,
             cityname: this.cityName,
             gender: this.gender,
             imageRoute: this.imageRoute,
