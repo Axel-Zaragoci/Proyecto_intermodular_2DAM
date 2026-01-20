5398e0385e071ed4bbce56cb12cab5ca5fa9c752
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-01-20 19:45:55 +0100

    Realizo el servicio de navegación

--- desktop/desktop_app/desktop_app/Services/NavigationService.cs
+++ desktop/desktop_app/desktop_app/Services/NavigationService.cs
@@ -1,7 +1,40 @@
-﻿namespace desktop_app.Services
-{
-    public class NavigationService
-    {
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using desktop_app.Views;
 
+namespace desktop_app.Services
+{
+    public class NavigationService : INotifyPropertyChanged
+    {
+        private static NavigationService? _instance;
+        public static NavigationService Instance => _instance ??= new NavigationService();
+
+        private List<UserControl> _stackViews = [new UserView()];
+        public UserControl CurrentView
+        {
+            get => _stackViews[0];
+            protected set
+            {
+                if (_stackViews[0] == value) return;
+                StackViews.Insert(0, value);
+            }
+        }
+
+        public List<UserControl> StackViews
+        {
+            get => _stackViews;
+        }
+
+        public void NavigateTo<T> () where T : UserControl, new()
+        {
+            CurrentView = StackViews.Find(e => e is T) ?? new T();
+            OnPropertyChanged(nameof(CurrentView));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ desktop/desktop_app/desktop_app/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using desktop_app.Commands;
+using desktop_app.Services;
+using System.Windows.Input;
+using desktop_app.Views;
+
+namespace desktop_app.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand ShowUsersCommand { get; } = new RelayCommand(_ => NavigationService.Instance.NavigateTo<UserView>());
+        public ICommand ShowBookingsCommand { get; } = new RelayCommand(_ => NavigationService.Instance.NavigateTo<BookingView>());
+        public ICommand ShowRoomsCommand { get; } = new RelayCommand(_ => NavigationService.Instance.NavigateTo<RoomView>());
+    }
+}
