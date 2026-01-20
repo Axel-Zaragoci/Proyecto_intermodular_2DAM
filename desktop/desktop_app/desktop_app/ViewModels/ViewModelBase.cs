1c73ad6d4ba62eceaca14d5b5da0e6dfbe7df167
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-01-20 20:52:28 +0100

    Documento la navegación usando C# XML Documentation comments

--- desktop/desktop_app/desktop_app/ViewModels/ViewModelBase.cs
+++ desktop/desktop_app/desktop_app/ViewModels/ViewModelBase.cs
@@ -2,10 +2,23 @@
 
 namespace desktop_app.ViewModels
 {
+    /// <summary>
+    /// Clase base para los ViewModels que implementa la interfaz <see cref="INotifyPropertyChanged"/> para notificar cambios en las propiedades a la vista
+    /// </summary>
     public class ViewModelBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Evento que se dispara cuando cambia el valor de una propiedad
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Notifica que una propiedad ha cambiado
+        /// </summary>
+        /// 
+        /// <param name="propertyName">
+        /// Nombre de la propiedad cuyo valor ha cambiado
+        /// </param>
         protected virtual void OnPropertyChanged(string propertyName)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
