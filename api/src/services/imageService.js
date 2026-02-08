7e6098ef246d1448a581d0f039c170395468097c
luislajo <luislajoborderias11@gmail.com>
2026-02-08 18:51:37 +0100

    room android / fix images

--- api/src/services/imageService.js
+++ api/src/services/imageService.js
@@ -25,5 +25,5 @@ const fileFilter = (req, file, cb) => {
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 25 * 1024 * 1024 }, // 25MB
 });
