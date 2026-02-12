0a04fe82cf7100b447aa4eaa04aec9aec17ead9c
IanRM-class <fp.ian.rodriguezmarhuenda@gmail.com>
2026-02-12 18:51:45 +0100

    Cambio temporal en MainActivity para simular el Logeo

--- mobile/Intermodular/app/src/main/java/com/example/intermodular/MainActivity.kt
+++ mobile/Intermodular/app/src/main/java/com/example/intermodular/MainActivity.kt
@@ -14,7 +14,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.intermodular.data.remote.RetrofitProvider
+import com.example.intermodular.data.repository.LoginRepository
 import com.example.intermodular.ui.theme.AppTheme
+import com.example.intermodular.viewmodels.LoginViewModel
+import com.example.intermodular.viewmodels.LoginViewModelFactory
 import com.example.intermodular.views.scaffold.MyApp
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +28,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val darkMode = rememberSaveable { mutableStateOf(false) }
+            val repo = LoginRepository(RetrofitProvider.api)
+            val factory =  LoginViewModelFactory(repo)
+            val vm: LoginViewModel = viewModel(factory = factory)
+            vm.login("cliente@prueba.test", "Password123!")
 
+            val darkMode = rememberSaveable { mutableStateOf(false) }
             AppTheme (darkTheme = darkMode.value) {
                 MyApp(
                     darkMode = darkMode.value,
